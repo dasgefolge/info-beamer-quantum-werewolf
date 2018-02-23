c687230a4f5e84003e2e88132e885c00da444523
Fenhl <fenhl@fenhl.net>
2018-02-23 22:41:41 +0000

    Load font

--- node.lua
+++ node.lua
@@ -5,6 +5,10 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 local json = require "json"
 local text = require "text"
 
+util.resource_loader{
+    "dejavu_sans.ttf"
+}
+
 local write = text{font=dejavu_sans, width=WIDTH, height=HEIGHT, r=1, g=1, b=1}
 
 local data = nil
